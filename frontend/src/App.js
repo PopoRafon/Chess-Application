06655c1fd727b596188509452c952451871ba3d5
PopoRafon <Leranomix1@wp.pl>
2023-09-19 22:36:39 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
