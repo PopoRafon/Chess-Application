9df4e93249a02ad5017ab3a4aab6a3fdf59ddff1
PopoRafon <Leranomix1@wp.pl>
2023-09-22 18:53:53 +0200

    Added board component and chess pieces

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,3 +1,5 @@
+import ChessBoard from './Board';
+
 function GameSidebar() {
     return (
         <div className="game-sidebar">
@@ -11,49 +13,12 @@ function GameSidebar() {
     );
 }
 
-function Column({ column, idx }) {
-    return (
-        <div>
-            {column.map((square, index) => (
-                <div className={(index + idx) % 2 === 1 ? 'black-square' : 'white-square'} key={index}>
-                    <div className="square-number">
-                        {idx === 0 ? index + 1 : ''}
-                    </div>
-                    <div className="square-letter">
-                        {index === 7 ? 'ABCDEFGH'[idx] : ''}
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-}
-
-function Board() {
-    const board = [
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0]]
-
-    return (
-        <div className="game-board">
-            {board.map((column, index) => (
-                <Column column={column} key={index} idx={index} />
-            ))}
-        </div>
-    );
-}
-
 function Game() {
     return (
         <div className="game-container">
             <div className="game-content">
                 <GameSidebar />
-                <Board />
+                <ChessBoard />
                 <GameSidebar />
             </div>
         </div>
