bc1fd87c9da2c1e6b7341e8323c1a5333870b8a9
PopoRafon <Leranomix1@wp.pl>
2023-09-27 02:35:50 +0200

    Added register form validation

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -10,7 +10,8 @@ function RegisterForm() {
         password2: '',
         checkbox: false
     });
-    
+    const [errors, setErrors] = useState({});
+
     function handleChange(event) {
         const { name, value, type, checked } = event.target;
 
@@ -27,26 +28,58 @@ function RegisterForm() {
         }
     }
 
+    function validation() {
+        const { email, username, password1, password2, checkbox } = formData;
+        const newErrors = {};
+
+        if (!email.match(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/)) newErrors['email'] = 'Enter a valid email address.';
+
+        if (email.length < 8) newErrors['email'] = 'Ensure this field has at least 8 characters.';
+
+        if (email.length > 64) newErrors['email'] = 'Ensure this field has no more than 64 characters.';
+
+        if (username.length < 8) newErrors['username'] = 'Ensure this field has at least 8 characters.';
+
+        if (username.length > 16) newErrors['username'] = 'Ensure this field has no more than 16 characters.';
+
+        if (password1.length < 8) newErrors['password1'] = 'Ensure this field has at least 8 characters.';
+
+        if (password1 !== password2) newErrors['password2'] = 'Passwords must be the same.';
+        
+        if (password2.length < 8) newErrors['password2'] = 'Ensure this field has at least 8 characters.';
+
+        if (!checkbox) newErrors['checkbox'] = 'Terms of Service must be accepted.';
+
+        if (Object.keys(newErrors).length >= 1) {
+            setErrors(newErrors);
+            return false;
+        }
+
+        return true;
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
 
-        fetch('/api/v1/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            if (data.success) {
-                navigate('/');
-            } else {
-                console.log(data);
-            }
-        });
+        if (validation()) {
+            fetch('/api/v1/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.success) {
+                    navigate('/');
+                } else {
+                    setErrors(data);
+                }
+            });
+        }
     }
 
     return (
@@ -62,8 +95,9 @@ function RegisterForm() {
                     name="email"
                     value={formData.email}
                     onChange={handleChange}
-                    className="register-form-input"
+                    className={errors.email ? "register-invalid-form-input" : "register-form-input"}
                 />
+                {errors.email && (<div className="invalid-field">{errors.email}</div>)}
             </div>
             <div className="register-form-input-container">
                 <label>Username</label>
@@ -72,8 +106,9 @@ function RegisterForm() {
                     name="username"
                     value={formData.username}
                     onChange={handleChange}
-                    className="register-form-input"
+                    className={errors.username ? "register-invalid-form-input" : "register-form-input"}
                 />
+                {errors.username && (<div className="invalid-field">{errors.username}</div>)}
             </div>
             <div className="register-form-input-container">
                 <label>Password</label>
@@ -82,8 +117,9 @@ function RegisterForm() {
                     name="password1"
                     value={formData.password1}
                     onChange={handleChange}
-                    className="register-form-input"
+                    className={errors.password1 ? "register-invalid-form-input" : "register-form-input"}
                 />
+                {errors.password1 && (<div className="invalid-field">{errors.password1}</div>)}
             </div>
             <div className="register-form-input-container">
                 <label>Confirm Password</label>
@@ -92,10 +128,11 @@ function RegisterForm() {
                     name="password2"
                     value={formData.password2}
                     onChange={handleChange}
-                    className="register-form-input"
+                    className={errors.password2 ? "register-invalid-form-input" : "register-form-input"}
                 />
+                {errors.password2 && (<div className="invalid-field">{errors.password2}</div>)}
             </div>
-            <div>
+            <div className="register-form-checkbox-container">
                 <label>
                     <input
                         type="checkbox"
@@ -105,6 +142,7 @@ function RegisterForm() {
                     />
                     Agree to Terms of Service
                 </label>
+                {errors.checkbox && (<div className="invalid-field">{errors.checkbox}</div>)}
             </div>
             <input
                 type="submit"
